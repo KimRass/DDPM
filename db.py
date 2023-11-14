358bce78981597350cb3fa0984cd35c5c1c3956c
KimRass <rmx1000@yonsei.ac.kr>
2023-11-14 19:06:22 +0900

    DuckDB

--- db.py
+++ db.py
@@ -5,20 +5,6 @@ import numpy as np
 
 db_path = "/Users/jongbeomkim/Downloads/flittopic_model/image.db"
 conn = duckdb.connect(database=db_path, read_only=False)
-conn.sql("SHOW TABLES").to_df()
-conn.table("image")
-conn.execute("SHOW TABLES").fetchdf()
-conn.execute("SHOW TABLES")
-conn.execute("SHOW TABLES").to_df()
-
-conn.execute("SHOW TABLES").fetchdf()["name"].tolist()
-
-df = pd.DataFrame({
-   'A': ['foo', 'bar', 'foo', 'bar', 'foo', 'bar', 'foo', 'foo'],
-   'B': ['one', 'one', 'two', 'three', 'two', 'two', 'one', 'three'],
-   'C': np.random.randn(8),
-   'D': np.random.randn(8)
-})
-result = duckdb.query("SELECT A, AVG(D) FROM df GROUP BY A").to_df()
-result
-
+temp = conn.table("image")
+df = conn.execute("SELECT * FROM temp").fetch_df()
+df["s3url"][5]
