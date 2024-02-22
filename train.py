898ae6e5596017dd923c59a6f13e4faabd06288f
KimRass <rmx1000@yonsei.ac.kr>
2024-02-22 18:30:37 +0900

    5 block_out_channels

--- train.py
+++ train.py
@@ -216,19 +216,17 @@ def main():
         in_channels=3,  # the number of input channels, 3 for RGB images
         out_channels=3,  # the number of output channels
         layers_per_block=2,  # how many ResNet layers to use per UNet block
-        block_out_channels=(128, 128, 256, 256, 512, 512),  # the number of output channes for each UNet block
+        block_out_channels=(128, 128, 256, 256, 512),
         down_block_types=( 
-            "DownBlock2D",  # a regular ResNet downsampling block
+            "DownBlock2D",
             "DownBlock2D", 
             "DownBlock2D", 
-            "DownBlock2D", 
-            "AttnDownBlock2D",  # a ResNet downsampling block with spatial self-attention
+            "AttnDownBlock2D",
             "DownBlock2D",
         ), 
         up_block_types=(
-            "UpBlock2D",  # a regular ResNet upsampling block
-            "AttnUpBlock2D",  # a ResNet upsampling block with spatial self-attention
             "UpBlock2D", 
+            "AttnUpBlock2D",
             "UpBlock2D", 
             "UpBlock2D", 
             "UpBlock2D"  
